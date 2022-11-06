6fd187bddb012060140ab490bf358463cfdf0f42
jeka0 <latosh02Wmail.ru>
2022-11-06 14:53:23 +0300

    added some blocks

---
+++ PCSystemInformation/SystemInformation/MotherboardInformation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCSystemInformation.SystemInformation
+{
+    public class MotherboardInformation: IMotherboardInformation
+    {
+        private RegistryAccess registry;
+        public MotherboardInformation()
+        {
+            registry = new RegistryAccess();
+        }
+    }
+}
